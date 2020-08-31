f0672efb7153e8d51f77637aedfef93ba2ae00b7
sgupta1903 <56866950+sgupta1903@users.noreply.github.com>
2020-08-31 16:44:43 +0530

    Add files via upload

---
+++ gradleselenium/src/test/java/com/example/julianharty/seleniumbasics/SeleniumBasic2.java
@@ -0,0 +1,30 @@
+package com.example.julianharty.seleniumbasics;
+
+//import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * Created by julianharty on 13/02/2017.
+ */
+
+public class SeleniumBasic2 {
+    @Test
+    public void helloGoogleTest2() throws Exception {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:5555/wd/hub"), capability);
+        driver.get("http://www.facebook.com");
+
+        WebDriver augmentedDriver = new Augmenter().augment(driver);
+        File screenshot = ((TakesScreenshot)augmentedDriver).
+                getScreenshotAs(OutputType.FILE);
+    }
+}
